589c3e06fd395ef907a031bddde62bb5e820d62a
JoseGabrielZeballos <a2019117789@estudiantes.upsa.edu.bo>
2020-09-19 08:00:52 -0400

    Ejercicio 2 sin jwt y sin el app service

---
+++ Preparcial2/Preparcial2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Preparcial2.Startup))]
+namespace Preparcial2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
